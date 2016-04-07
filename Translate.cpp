c25aaea981d39966983a4b699a4b9b32b5dc85cd
danesgo <degs432@hotmail.com>
2016-04-06 22:13:33 -0600

    nonlinear function finished but not tested

--- Translate.cpp
+++ Translate.cpp
@@ -1,12 +1,6 @@
 
-
-def nonlin(x,deriv=False):
-    if(deriv==True):
-        return x*(1-x)
-    return 1/(1+np.exp(-x))
-
 double nonlin(double x, bool deriv){
-  if(deriv==True){
+  if(deriv==true){
     return x*(1-x)
   }
   return 1/(1 + exp(-x))
